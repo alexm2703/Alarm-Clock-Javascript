9a00961714abb6c1166bcbd296a7598acceca409
alexm2703 <alex-marleau@hotmail.com>
2017-05-02 09:19:55 -0400

    Create horloge.js

---
+++ horloge.js
@@ -0,0 +1,43 @@
+// Afficher l'heure sur la page.
+function afficherHeure() {
+var date = new Date();
+var ajouterZeroMinute;
+var ajouterZeroSeconde;
+
+if(date.getMinutes()<10){ ajouterZeroMinute = "0"; } else { ajouterZeroMinute = ""; }
+if(date.getSeconds()<10){ ajouterZeroSeconde = "0"; } else { ajouterZeroSeconde = ""; }
+
+document.getElementById("span_horloge").textContent = date.getHours() + ":" + ajouterZeroMinute + date.getMinutes() + ":" + ajouterZeroSeconde + date.getSeconds();
+};
+setInterval('afficherHeure()', 1000);
+
+// Ajouter une alarme
+function setAlarme() {
+document.getElementById("btn_ajouterAlarme").disabled = true;
+document.getElementById("btn_arreterAlarme").disabled = false;
+
+var getAlarmeHeure = document.getElementById("select_alarmeHeures");
+var setAlarmeHeure = getAlarmeHeure.options[getAlarmeHeure.selectedIndex].value;
+
+var getAlarmeMinute = document.getElementById("select_alarmeMinutes");
+var setAlarmeMinute= getAlarmeMinute.options[getAlarmeMinute.selectedIndex].value;
+
+var getAlarmeSeconde = document.getElementById("select_alarmeSecondes");
+var setAlarmeSeconde = getAlarmeSeconde.options[getAlarmeSeconde.selectedIndex].value
+
+monAlarmeJSon = {"heure":setAlarmeHeure, "minute":setAlarmeMinute, "seconde":setAlarmeSeconde};
+//alert("Une alarme sonnera dans: " + monAlarmeJSon.heure +  " heure(s) " + monAlarmeJSon.minute + " minute(s) et " + monAlarmeJSon.seconde + " seconde(s)");
+
+var calculTemps = (monAlarmeJSon.heure * 3600000) + (monAlarmeJSon.minute * 60000) + (monAlarmeJSon.seconde * 1000);
+setTimeout('JouerSon()', calculTemps);
+};
+
+function JouerSon(){
+	document.getElementById('buzzer').play();
+};
+
+function Arreter(){
+	document.getElementById('buzzer').pause();
+	document.getElementById("btn_ajouterAlarme").disabled = false;
+	document.getElementById("btn_arreterAlarme").disabled = true;
+};
